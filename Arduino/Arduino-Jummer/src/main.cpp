786d3114988227d04fde9e9d4f78ba510a85bb0b
Valdenir Severino <vald3nir@gmail.com>
2025-03-08 19:46:41 -0300

    up

--- Arduino/Arduino-Jummer/src/main.cpp
+++ Arduino/Arduino-Jummer/src/main.cpp
@@ -1,49 +1,71 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include <Arduino.h>
+#include "RF24.h"
 
-/*
-Conexões do nRF24L01 com o Arduino Uno
-nRF24L01	            Arduino Uno
-VCC (3.3V)	            3.3V (NÃO usar 5V!)
-GND	                    GND
-CE	                    9
-CSN	                    10
-SCK	                    13
-MOSI	                11
-MISO	                12
-IRQ (não obrigatório)	-
-*/
+SPIClass *hp = nullptr;
+RF24 radio(9, 10, 16000000);
+byte i = 45;
+unsigned int flag = 0;
 
-#define CE_PIN 9   // Pino CE no Arduino Uno
-#define CSN_PIN 10 // Pino CSN (SS)
+void initHP()
+{
+    if (radio.begin())
+    {
+        delay(200);
+        Serial.println("Hp Started !!!");
+        radio.setAutoAck(false);
+        radio.stopListening();
+        radio.setRetries(0, 0);
+        radio.setPayloadSize(5);
+        radio.setAddressWidth(3);
+        radio.setPALevel(RF24_PA_MAX, true);
+        radio.setDataRate(RF24_2MBPS);
+        radio.setCRCLength(RF24_CRC_DISABLED);
+        radio.printPrettyDetails();
+        radio.startConstCarrier(RF24_PA_MAX, i);
+    }
+    else
+    {
+        Serial.println("HP couldn't start !!");
+    }
+}
 
-RF24 radio(CE_PIN, CSN_PIN);
+void two()
+{
+    if (flag == 0)
+    {
+        i += 2;
+    }
+    else
+    {
+        i -= 2;
+    }
+    if ((i > 79) && (flag == 0))
+    {
+        flag = 1;
+    }
+    else if ((i < 2) && (flag == 1))
+    {
+        flag = 0;
+    }
+    radio.setChannel(i);
+}
+
+void one()
+{
+    for (int i = 0; i < 79; i++)
+    {
+        radio.setChannel(i);
+    }
+}
 
 void setup()
 {
     Serial.begin(115200);
-    if (!radio.begin())
-    {
-        Serial.println("Falha ao inicializar o nRF24L01!");
-    }
-
-    radio.setPALevel(RF24_PA_MAX);
-    radio.stopListening(); // Define como transmissor
+    initHP();
 }
 
 void loop()
 {
-    byte payload[32];
-    memset(payload, 0xFF, sizeof(payload)); // Preenche com 0xFF
-
-    for (int i = 0; i <= 125; i++) // Testa canais Bluetooth (0 a 125)
-    {
-        radio.setChannel(i);
-        Serial.print("Enviando pacotes no canal: ");
-        Serial.println(i);
-
-        radio.write(payload, sizeof(payload)); // Envia pacotes
-        delayMicroseconds(100);                // Pequeno atraso para sobrecarregar o canal
-    }
+     two();
+    //one();
 }
